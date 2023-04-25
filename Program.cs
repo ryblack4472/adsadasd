66f391b3837420f03f3d991387f397ced4187263
ryblack4472 <123537397+ryblack4472@users.noreply.github.com>
2023-04-25 23:52:11 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿class MainClass {
+  public static void Main (string[] args) {
+    // 5.1.1 - รับจำนวนไอเทมในกระเป๋าของผู้เล่น
+    Console.Write("กรุณากรอกจำนวนไอเทมในกระเป๋าของคุณ: ");
+    int numItems = int.Parse(Console.ReadLine());
+
+    // 5.1.2 - รับรายละเอียดไอเทมทีละชิ้นจนครบตามจำนวนที่กรอก
+    Dictionary<string, string> items = new Dictionary<string, string>();
+    for (int i = 1; i <= numItems; i++) {
+      Console.Write($"กรุณากรอกชื่อไอเทมที่ {i}: ");
+      string name = Console.ReadLine();
+
+      Console.Write($"กรุณากรอกประเภทของไอเทม \"{name}\": ");
+      string type = Console.ReadLine();
+
+      items[name] = type;
+    }
+
+    // 5.2 - รับโหมดการค้นหาไอเทม
+    while (true) {
+      Console.Write("กรุณากรอกโหมดการค้นหา (หรือ \"End\" เพื่อออก): ");
+      string mode = Console.ReadLine();
+
+      if (mode == "End") {
+        Console.WriteLine("End");
+        break;
+      } else if (mode == "ShowAll") {
+        Console.WriteLine("ไอเทมในกระเป๋าของคุณ:");
+        foreach (KeyValuePair<string, string> item in items) {
+          Console.WriteLine($"{item.Key} ({item.Value})");
+        }
+      } else {
+        Console.WriteLine($"ไอเทมประเภท \"{mode}\" ในกระเป๋าของคุณ:");
+        foreach (KeyValuePair<string, string> item in items) {
+          if (item.Value == mode) {
+            Console.WriteLine(item.Key);
+          }
+        }
+      }
+
+      Console.WriteLine();
+    }
+  }
+}
